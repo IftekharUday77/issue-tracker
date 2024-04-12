3aa2409152221058a4d83da6a54be2d65e4f96d6
uday <uday14@live.com>
2024-04-12 16:07:59 +0600

    Made Columns Actually Sortable

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -11,21 +11,28 @@ const IssuesPage = async ({
 }: {
   searchParams: { status: Status; orderBy: keyof Issue };
 }) => {
-  const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
-
   const columns: { label: string; value: keyof Issue; className?: string }[] = [
     { label: "Issue", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
 
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (
